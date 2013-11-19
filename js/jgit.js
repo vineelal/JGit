e926d62a67ee5613ab902e32f635709203ee6029
vineelal <vineelal@vineela-l.(none)>
2013-11-19 19:45:05 +0530

    trying the cli UI

--- js/jgit.js
+++ js/jgit.js
@@ -2,10 +2,31 @@
  * Created by vineelal on 18/11/13.
  */
 function nextLine(e){
-    var cmd = document.getElementById('cmd');
-    var user = document.getElementById('user');
     if(e.keyCode === 13){
-        user.insertAdjacentHTML('afterend','<div id="user">vineelal@vineela-l:~$<input id="cmd" type="text" onkeypress="nextLine(event)"></div>');
+           /* user.insertAdjacentHTML('afterend','<div id="user">vineelal@vineela-l:~$<input id="cmd" type="text" onkeypress="nextLine(event)"></div>');
+            console.log("The command to be executed "+cmd.value);*/
+        var command = document.getElementById("input").value;
+        var output = executeCommand(command);
+        document.getElementById("output").value = output;
+        var div = document.createElement("div");
+        div.setAttribute("id","user");
+        var text = document.createTextNode("vineelal@vineela-l:~$");
+        var input = document.createElement("input");
+        input.setAttribute("onkeypress","nextLine(e)")
+//        input.onkeypress = nextLine(e);
+        input.addEventListener("onkeypress", function(){nextLine(event)}, false);
+        var output = document.createElement("input");
+        div.appendChild(text);
+        div.appendChild(input);
+        div.appendChild(output);
+        document.body.appendChild(div);
     }
+   /* else{
+        var length = (cmd.value.length+1)*8;
+        cmd.setAttribute('width',length);
+    }*/
+}
 
+function executeCommand(input){
+    return "the output of the command "+input;
 }
